0b48f6c3cc18f34dbce4914fe10017bf838f4736
jeanking23 <jeandedieu405@gmail.com>
2023-11-09 00:43:06 -0500

    flask  install

--- Backend/env/Scripts/activate.bat
+++ Backend/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jeand\Documents\GitHub\Subscrition-Box-service-Capstone-Project-\backend\env
+set VIRTUAL_ENV=C:\Users\jeand\Documents\Work project\Subscrition-Box-service-Capstone-Project-\backend\env
 
 if not defined PROMPT set PROMPT=$P$G
 
